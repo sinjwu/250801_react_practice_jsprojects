3361bc62d3f55102f4d9bb921d67ab8adcd7fd81
sinjwu <harbirdouk@gmail.com>
2025-08-01 09:51:34 +0900

    250801fc

--- react_boilerplate/src/App.jsx
+++ react_boilerplate/src/App.jsx
@@ -1,5 +1,11 @@
+import Counter from "./components/Counter";
+
 const App = () => {
-  return <div className="bg-red-100">App</div>;
+  return (
+    <div className="bg-red-100">
+      <Counter />
+    </div>
+  );
 };
 
 export default App;
